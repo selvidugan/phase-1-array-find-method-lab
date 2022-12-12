9f65908ae00b4340097ac9f87f550a4eacf28cbc
selvidugan <selvidugan@gmail.com>
2022-12-12 13:38:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+// function superbowlWin(record) {
+//     return record.result === "W" 
+// }
+
+// const year = record.find(superbowlWin).year;
+
+superbowlWin = (record) => {
+
+    const result = record.find( record => record.result === "W" );
+    
+ 
+    return !!result ? result.year : undefined;
+  }
